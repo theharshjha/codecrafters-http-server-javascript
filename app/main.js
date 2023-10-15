4f48f37e24c654b4d5fa1f4ee11463f5edd6eb14
imtheonly1 <imtheonly1.in@gmail.com>
2023-10-15 19:33:29 +0530

    passed 3rd stage

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 const net = require("net");
 const server = net.createServer((socket) => {
     socket.on("data", 'utf-8', (req) => {
+        console.log(req);
         if (req.method == 'GET' && req.path == '/') {
             socket.write("HTTP/1.1 200 OK\r\n\r\n");
         } else {
