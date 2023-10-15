247768a91dd2a00c395449c3be5d0f194b23ca31
imtheonly1 <imtheonly1.in@gmail.com>
2023-10-15 21:28:56 +0530

    passed 4th stage

--- app/main.js
+++ app/main.js
@@ -1,37 +1,39 @@
+const { Socket } = require("dgram");
 const net = require("net");
 const server = net.createServer((socket) => {
+    socket.on('close', () => {
+        socket.end();
+        server.closed();
+    });
     socket.on('data', (req) => {
-        req = parse(req.toString('utf-8'));
-        let PATH = req.path.split('/');
-        console.log(PATH);
-        if (req.method == 'GET' && req.path == '/') {
-            socket.write("HTTP/1.1 200 OK\r\n\r\n");
-        } else if (req.method == 'GET' && PATH[1] == 'echo') {
-            socket.write("HTTP/1.1 200 OK\r\n\r\n");
-            socket.write("Content-Type: text/plain\r\n\r\n");
-            socket.write("Content-Length: {PATH[3].length}\r\n\r\n");
-            socket.write("{PATH[3]}\r\n\r\n");
-        } else {
-            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-        }
+        req = req.toString('utf-8');
+        parse(req);
+        let res = 'HTTP/1.1';
+        socket.write(res);
         socket.end();
-    })
-    socket.on("close", () => {
-        socket.end();
-        server.close();
     });
 });
-const parse = (requestString) => {
-    const lines = requestString.split("\r\n")
+const parse  = (req) => {
+    const lines = req.split('\r\n');
     console.log(lines);
     const [startLines] = lines;
     console.log(startLines);
-    const [method, path, protocol] = startLines.split(" ")
-    return {
-      method,
-      path,
-      protocol
-    }
-  }
+}
+// const parseRequest = (requestString) => {
 
-server.listen(4221, "localhost");
+//     const lines = requestString.split("\r\n")
+
+//     const [startLines] = lines;
+
+//     const [method, path, protocol] = startLines.split(" ")
+
+//     return {
+
+//         method,
+
+//         path,
+
+//         protocol
+
+//     }
+    server.listen(4221, "localhost");
