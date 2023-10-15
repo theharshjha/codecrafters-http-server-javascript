bb918a02ec260cb31d39046cdc255ce6996b415a
imtheonly1 <imtheonly1.in@gmail.com>
2023-10-15 21:10:32 +0530

    passed 4th stage

--- app/main.js
+++ app/main.js
@@ -2,8 +2,12 @@ const net = require("net");
 const server = net.createServer((socket) => {
     socket.on('data', (req) => {
         req = parse(req.toString('utf-8'));
-        if (req.method == 'GET' && req.path == '/') {
+        let PATH = req.path.split('/');
+        if (req.method == 'GET' && PATH[1] == 'echo') {
             socket.write("HTTP/1.1 200 OK\r\n\r\n");
+            socket.write("Content-Type: text/plain");
+            socket.write("Content-Length: ", PATH[3].length);
+            socket.write(PATH[3]);
         } else {
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
         }
@@ -16,23 +20,14 @@ const server = net.createServer((socket) => {
     });
 });
 const parse = (requestString) => {
-
     const lines = requestString.split("\r\n")
-  
     const [startLines] = lines;
-  
     const [method, path, protocol] = startLines.split(" ")
-  
     return {
-  
       method,
-  
       path,
-  
       protocol
-  
     }
-  
   }
 
 server.listen(4221, "localhost");
