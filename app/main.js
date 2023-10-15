24c143a628a80332f5c5db3ca1304c82236a38fa
imtheonly1 <imtheonly1.in@gmail.com>
2023-10-16 00:00:41 +0530

    passed 7th stage

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const fs = require('fs');
 const server = net.createServer((socket) => {
     socket.on('close', () => {
         socket.end();
@@ -21,6 +22,19 @@ const server = net.createServer((socket) => {
                 req.path = parsePath(req.path);
                 socket.write(`Content-Length: ${req.path.data.length}\r\n\r\n`);
                 socket.write(req.path.data);
+            } else if (req.path.split('/')[1] == 'files') {
+                const file = req.path.split('/')[2];
+                const dir = process.argv[3];
+                const files = fs.readdirSync(dir);
+                if (!files.include(file)) {
+                    socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+                } else {
+                    const data = fs.readfileSync(`${dir}/${file}`, 'utf-8');
+                    ocket.write('HTTP/1.1 200 OK\r\n');
+                    socket.write('Content-Type: application/octet-stream\r\n');
+                    socket.write(`Content-Length: ${data.length}\r\n\r\n`);
+                    socket.write(data);
+                }
             } else {
                 socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
             }
