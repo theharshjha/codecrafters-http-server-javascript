d91dc9392133c0db3319b0245264ba82ace842cb
imtheonly1 <imtheonly1.in@gmail.com>
2023-10-15 23:39:08 +0530

    passed 5th stage

--- app/main.js
+++ app/main.js
@@ -10,12 +10,17 @@ const server = net.createServer((socket) => {
         if (req.method == 'GET') {
             if (req.path == '/') {
                 socket.write('HTTP/1.1 200 OK\r\n\r\n');
-            } else if (req.path.split('/')[1] == 'user-agent' || req.path.split('/')[1] == 'echo') {
+            } else if (req.path.split('/')[1] == 'user-agent') {
                 socket.write('HTTP/1.1 200 OK\r\n');
                 socket.write('Content-Type: text/plain\r\n');
                 socket.write(`Content-Length: ${req.data.length}\r\n\r\n`);
                 socket.write(req.data);
-
+            } else if (req.path.split('/')[1] == 'echo') {
+                socket.write('HTTP/1.1 200 OK\r\n');
+                socket.write('Content-Type: text/plain\r\n');
+                req.path = parsePath(req.path);
+                socket.write(`Content-Length: ${req.path.data.length}\r\n\r\n`);
+                socket.write(req.path.data);
             } else {
                 socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
             }
@@ -25,6 +30,10 @@ const server = net.createServer((socket) => {
         socket.end();
     });
 });
+const parsePath = (path) => {
+    const data = path.split('/').slice(2).join('/');
+    return {data};
+}
 const parseReq = (req) => {
     const lines = req.split('\r\n');
     const [firstLine, , thirdLine] = lines;
