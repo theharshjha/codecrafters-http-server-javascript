7163a9beb8d544d3b2db4d774282a8375b6799c8
imtheonly1 <imtheonly1.in@gmail.com>
2023-10-15 19:38:24 +0530

    passed 3rd stage

--- app/main.js
+++ app/main.js
@@ -2,14 +2,25 @@ const net = require("net");
 const server = net.createServer((socket) => {
     socket.on("data", (req) => {
         console.log(req);
-        req = req.toString('utf-8');
+        req = parseRequest(req.toString('utf-8'));
         if (req.method == 'GET' && req.path == '/') {
             socket.write("HTTP/1.1 200 OK\r\n\r\n");
         } else {
             socket.write("HTTP/1.1 404 Not Found");
         }
         socket.end();
+        server.close();
     });
+    const parseRequest = (requestString) => {
+        const lines = requestString.split("\r\n")
+        const [startLines] = lines;
+        const [method, path, protocol] = startLines.split(" ")
+        return {
+            method,
+            path,
+            protocol
+        }
+    }
     socket.on("close", () => {
         socket.end();
         server.close();
